334f530e2d56cbc5ffc8ddce837535ae019bf8c1
Home <mohammedis271@gmail.com>
2019-06-07 18:49:04 +0200

    Test

---
+++ app/src/main/java/com/example/gittest/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.gittest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageButton img;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
